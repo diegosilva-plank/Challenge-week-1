30bef8c030150ac03fab3ff057b0be6b5565cec7
arthurbraga-plank <arthurmartinsbraga@MacBook-Air-de-Arthur.local>
2023-04-14 08:21:59 -0300

    formatting

--- src/repositories/genericRepository.ts
+++ src/repositories/genericRepository.ts
@@ -1,52 +1,59 @@
-import { jsonServer } from "../services/api"
+import { jsonServer } from "../services/api";
 
-export class Repository<T> { 
-    
-    entityName: string
+export class Repository<T> {
+  entityName: string;
 
-    constructor(e: string) {
-        this.entityName = e
-    }
+  constructor(e: string) {
+    this.entityName = e;
+  }
 
-    async get (filter?: Partial<T>): Promise<T[]> {
-        try {
-            const response = await jsonServer.get<T[]>(`/${this.entityName}`, { params: filter })
-            return response.data
-        } catch (err) {
-            console.error(err)
-            throw err
-        }
+  async get(filter?: Partial<T>): Promise<T[]> {
+    try {
+      const response = await jsonServer.get<T[]>(`/${this.entityName}`, {
+        params: filter,
+      });
+      return response.data;
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
-    async create (entity: T): Promise<T> {
-        try {
-            const response = await jsonServer.post<T>(`/${this.entityName}`, entity)
-            return response.data
-        } catch (err) {
-            console.error(err)
-            throw err
-        }
+  }
+  async create(entity: T): Promise<T> {
+    try {
+      const response = await jsonServer.post<T>(`/${this.entityName}`, entity);
+      return response.data;
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
-    async update<T>(id: string, entity : Partial<Omit<T, 'id'>>): Promise<T> {
-        try {
-            const responseGet = await jsonServer.get<T[]>(`/${this.entityName}`, { params: { id } })
-            const [ complete ] = responseGet.data
-            const updated: T = {...complete, ...entity}
-            const response = await jsonServer.put<T>(`/${this.entityName}/${id}`, updated)
-            return response.data
-        } catch (err) {
-            console.error(err)
-            throw err
-        }
+  }
+  async update<T>(id: string, entity: Partial<Omit<T, "id">>): Promise<T> {
+    try {
+      const responseGet = await jsonServer.get<T[]>(`/${this.entityName}`, {
+        params: { id },
+      });
+      const [completeEntity] = responseGet.data;
+      const updated: T = { ...completeEntity, ...entity };
+
+      const response = await jsonServer.put<T>(
+        `/${this.entityName}/${id}`,
+        updated
+      );
+      return response.data;
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
-    async delete<T>(id: string): Promise<boolean> {
-        try {
-            await jsonServer.delete(`/${this.entityName}/${id}`)
-            return true
-        } catch (err) {
-            console.error(err)
-            throw err
-        }
+  }
+  async delete(id: string): Promise<boolean> {
+    try {
+      await jsonServer.delete(`/${this.entityName}/${id}`);
+      return true;
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
+  }
 }
 
 // Promise<T[]>
@@ -55,7 +62,7 @@ export class Repository<T> {
 //     get<T>(filter?: Partial<T>): Promise<T[]>
 //     create<T>(data : T): Promise<T>
 //     update<T>(id: string, data : Partial<Omit<T, 'id'>>): Promise<T>
-//     delete<T>(id: string): Promise<boolean> 
+//     delete<T>(id: string): Promise<boolean>
 // }
 
 // type Repository<T> = {
@@ -73,26 +80,26 @@ export class Repository<T> {
 //             throw err
 //         }
 //     },
-    // createRocket: async (rocket) => {
-    //     try {
-    //         const response = await jsonServer.post<Rocket>('/rocket', rocket)
-    //         return response.data
-    //     } catch (err) {
-    //         console.error(err)
-    //         throw err
-    //     }
-    // },
-    // updateRocket: async (id, rocket) => {
-    //     try {
-    //         const [ rocketComplete ] = await RocketRepository.getRockets({ id })
-    //         const updatedRocket: Rocket = {...rocketComplete, ...rocket}
-    //         const response = await jsonServer.put<Rocket>(`/rocket/${id}`, updatedRocket)
-    //         return response.data
-    //     } catch (err) {
-    //         console.error(err)
-    //         throw err
-    //     }
-    // },
+// createRocket: async (rocket) => {
+//     try {
+//         const response = await jsonServer.post<Rocket>('/rocket', rocket)
+//         return response.data
+//     } catch (err) {
+//         console.error(err)
+//         throw err
+//     }
+// },
+// updateRocket: async (id, rocket) => {
+//     try {
+//         const [ rocketComplete ] = await RocketRepository.getRockets({ id })
+//         const updatedRocket: Rocket = {...rocketComplete, ...rocket}
+//         const response = await jsonServer.put<Rocket>(`/rocket/${id}`, updatedRocket)
+//         return response.data
+//     } catch (err) {
+//         console.error(err)
+//         throw err
+//     }
+// },
 //     deleteRocket: async (id) => {
 //         try {
 //             await jsonServer.delete(`/rocket/${id}`)
@@ -103,4 +110,3 @@ export class Repository<T> {
 //         }
 //     }
 // }
-
