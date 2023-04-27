4c129ac66226728be830df67784bc14352f69c7c
arthurbraga-plank <arthur.braga@joinplank.com>
2023-04-27 14:51:24 -0300

    sequencializa

--- server.ts
+++ server.ts
@@ -13,23 +13,28 @@ Sentry.init({
   dsn: "https://02afaa85ede049c38097f23c0023fff4@o4505081419661312.ingest.sentry.io/4505081421824000",
 });
 
-app.use(Sentry.Handlers.requestHandler());
-
-app.use(express.json());
-app.use(router);
-
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
-app.use(Sentry.Handlers.errorHandler());
-app.use(errors());
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
+initializeDatabase()
   .catch((err) => {
     console.error("Error during Data Source initialization", err);
-  });
+  })
+  .then(initializeApp);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+function initializeApp() {
+  app.use(Sentry.Handlers.requestHandler());
+
+  app.use(express.json());
+  app.use(router);
+
+  app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+  app.use(Sentry.Handlers.errorHandler());
+  app.use(errors());
+
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+}
+
+async function initializeDatabase() {
+    await AppDataSource.initialize()
+    console.log("Data Source has been initialized!");
+}
